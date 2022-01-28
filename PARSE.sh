d19bbfa6db08fc927ba41dcf9a63fea5c185f005
derpferpmerp <cmfleming@eths202.org>
2022-01-27 23:55:45 -0600

    Renamed Dog Images

--- PARSE.sh
+++ PARSE.sh
@@ -1,16 +1,16 @@
 cd "$1"
 number=0
-mkdir ../OUTPUT
 for inputfile in ./*; do
-	outputfile="${inputfile%.*}.jpg"
-	filename=$(basename -- "$inputfile")
-	extension="${filename##*.}"
-	if [[ $extension == "heic" ]]; then
-		heif-convert -q 100 $inputfile "../OUTPUT/${inputfile%.*}.jpg"
-	else
-		convert "$inputfile" "$outputfile" &&
-		[[ -e "$outputfile" && "$inputfile" != "$outputfile" ]]
-		mv $outputfile "../OUTPUT/${inputfile%.*}.jpg"
-	fi
+	#outputfile="${inputfile%.*}.jpg"
+	#filename=$(basename -- "$inputfile")
+	#extension="${filename##*.}"
+	#if [[ $extension == "heic" ]]; then
+	#	heif-convert -q 100 $inputfile "../OUTPUT/${inputfile%.*}.jpg"
+	#else
+	#	convert "$inputfile" "$outputfile" &&
+	#	[[ -e "$outputfile" && "$inputfile" != "$outputfile" ]]
+	#	mv $outputfile "../OUTPUT/${inputfile%.*}.jpg"
+	#fi
+	mv $inputfile "./dog${number}.jpg"
 	number=$((number + 1))
 done
